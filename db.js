3a8f0ff732df08453ddc9d24fb5813b6d3f5254b
Mallick-Satyam <mallick.satyam072002@gmail.com>
2023-12-20 00:07:01 +0530

    Initial Commit

---
+++ db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+function connectDB(){
+
+    mongoose.connect('mongodb+srv://mallicksatyam072002:rIwiSMEo1nS2gy3H@cluster0.2atws8d.mongodb.net/?retryWrites=true&w=majority' , {useUnifiedTopology: true , useNewUrlParser: true})
+
+    const connection = mongoose.connection
+
+    connection.on('connected' , ()=>{
+        console.log('Mongo DB Connection Successfull')
+    })
+
+    connection.on('error' , ()=>{
+        console.log('Mongo DB Connection Error')
+    })
+
+
+}
+
+connectDB()
+
+module.exports = mongoose
